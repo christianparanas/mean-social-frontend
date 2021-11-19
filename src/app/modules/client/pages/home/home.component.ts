ee54322004951ad4497e8a8036337effcc9c2369
kate <christiannparanas@gmail.com>
2021-11-19 20:06:05 +0800

    post

--- src/app/modules/client/pages/home/home.component.ts
+++ src/app/modules/client/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  postArr: any = new Array(4)
 
   constructor() { }
 
