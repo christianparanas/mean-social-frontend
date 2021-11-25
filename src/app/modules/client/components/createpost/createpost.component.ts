614ed0fcbc7e4d20ff5c921e10d7d4f04ed752dd
kate <christiannparanas@gmail.com>
2021-11-25 17:52:18 +0800

    create post modal

--- src/app/modules/client/components/createpost/createpost.component.ts
+++ src/app/modules/client/components/createpost/createpost.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createpost.component.scss']
 })
 export class CreatepostComponent implements OnInit {
-  isCreatePostModalOpen: boolean = false
+  isCreatePostModalOpen: boolean = true
 
   constructor() { }
 
