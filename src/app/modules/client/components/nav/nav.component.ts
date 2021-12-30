26b2c0ef8df17d12487aa7e9c749d17dcb99f2d1
kate <christiannparanas@gmail.com>
2021-12-30 17:48:17 +0800

    :recycle: refactor: add sidebar shortcuts

--- src/app/modules/client/components/nav/nav.component.ts
+++ src/app/modules/client/components/nav/nav.component.ts
@@ -6,7 +6,37 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-  sidebarOpen = false;
+  sidebarOpen = true;
+  shortcutsArray: any = [
+    {
+      icon: "fal fa-flag",
+      title: "Pages"
+    },
+    {
+      icon: "fal fa-users",
+      title: "Groups"
+    },
+    {
+      icon: "fal fa-calendar",
+      title: "Events"
+    },
+    {
+      icon: "fal fa-virus",
+      title: "COVID-19 Information Center"
+    },
+    {
+      icon: "fal fa-briefcase",
+      title: "Jobs"
+    },
+    {
+      icon: "fal fa-clock",
+      title: "Memories"
+    },
+    {
+      icon: "fal fa-user",
+      title: "Find friends"
+    },
+  ]
 
   @Input() currentRoute: string;
 
