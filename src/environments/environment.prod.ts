f0a818f72f71dbbb416fa1fa4be5e958de631ed0
kate <christiannparanas@gmail.com>
2021-12-30 20:44:27 +0800

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  siteURL: 'http://localhost:4200',
+  baseURL: 'http://localhost:3000',
+  supabaseUrl: 'https://hzniiuicewjjaujxpnlm.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODA5MzAxMSwiZXhwIjoxOTUzNjY5MDExfQ.L8O1DtRP6aUN8iU3heFpvT-NIH85YiipjTHd6eRBqnM',
 };
